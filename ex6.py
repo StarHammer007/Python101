4c1b3ec831db5b07cf26652cca16fbb0021b988c
Jestin Roy <42301788+StarHammer007@users.noreply.github.com>
2019-08-16 16:52:34 +0530

    Sixth Excercise

---
+++ ex6.py
@@ -0,0 +1,12 @@
+hilarious=False
+jokeevaluation="Isn't that joke funny???! {} "
+print(jokeevaluation.format(hilarious))
+
+w="This is the left side..."
+e="of a string with righht side."
+
+print(w+e)
+
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that mary went.")
+print("."*10)
