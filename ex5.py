106bf1266a474569d1e3f88fcb37c98dd101e579
Jestin Roy <42301788+StarHammer007@users.noreply.github.com>
2019-08-16 16:52:18 +0530

    Fifth Excercise

---
+++ ex5.py
@@ -0,0 +1,13 @@
+my_name='Jestin Roy'
+my_age=20
+my_height=178
+my_eyes='Brown af'
+my_teeth='crystal white'
+
+print(f"Lets talk about {my_name} .")
+print(f"He's about {my_height} cm tall")
+print(f"He's {my_age} years  old.")
+print(f"He has {my_eyes} colour eyes.")
+print(f"His teeth are usually {my_teeth} except when ingesting coffee.")
+total=my_age+my_height
+print(f"If I add {my_age},{my_height},I get total {total}.")
