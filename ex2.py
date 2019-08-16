2559a761f5071865c2cac293c6637a09c96831c8
Jestin Roy <42301788+StarHammer007@users.noreply.github.com>
2019-08-16 16:50:55 +0530

    Second Exercise

---
+++ ex2.py
@@ -0,0 +1,18 @@
+print("HELLO WORLD!")
+print("hello again")
+#print("i like typing like this")
+#this is fun
+#print("THIS WILL RUN")
+print("this will run baby")
+
+
+
+print("I wil now count my chickens")
+print("HENS", 25+45 /2)
+print("I will now count my eggs")
+
+print(3+2+1-5+4%2-1/4+6)
+print("Is it true that 3+2 < 5-7 ?")
+
+print(3+2 <5-7)
+print("How about some more fun?")
