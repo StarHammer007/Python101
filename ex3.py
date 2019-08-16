4ca80cf27bbd3242df6fdfa843ddbe384339a147
Jestin Roy <42301788+StarHammer007@users.noreply.github.com>
2019-08-16 16:51:20 +0530

    Third Excercise

---
+++ ex3.py
@@ -0,0 +1,34 @@
+print("HELLO WORLD!")
+print("hello again")
+#print("i like typing like this")
+#this is fun
+#print("THIS WILL RUN")
+print("this will run baby")
+
+
+
+print("I wil now count my chickens")
+print("HENS", 25+45 /2)
+print("I will now count my eggs")
+
+print(3+2+1-5+4%2-1/4+6)
+print("Is it true that 3+2 < 5-7 ?")
+
+print(3+2 <5-7)
+print("How about some more fun?")
+
+cars=100
+space_in_car=5.0
+drivers=30
+passengers=90
+cars_not_driven=cars-drivers
+cars_driven=drivers
+carpool_capacity=cars_driven*space_in_car
+average_passengers=passengers/cars_driven
+print("There are",cars,"cars available in the pool.")
+
+print("There are only",drivers,"drivers available.")
+
+print("We can transport",carpool_capacity,"people today.")
+print("WE have ",passengers,"passengers")
+print("we need to put about",average_passengers,"in a car.")
